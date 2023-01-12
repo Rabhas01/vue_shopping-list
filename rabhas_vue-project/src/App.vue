0abce68910fe6e78591ba31bfbf32cb9032f8c4d
Rabhas01 <rabhas.vashisht@gmail.com>
2023-01-11 21:08:06 -0500

    make the content of shopping list interactable

--- rabhas_vue-project/src/App.vue
+++ rabhas_vue-project/src/App.vue
@@ -5,7 +5,7 @@ const header = ref("Shopping List App");
 const editing = ref(false);
 const items = ref([
   { label: "Rice", purchased: true, highpriority: true },
-  { label: "Carrots", purchased: true, highpriority: false },
+  { label: "Carrots", purchased: false, highpriority: false },
   { label: "Tofu", purchased: false, highpriority: true },
 ]);
 const newItem = ref("");
@@ -15,18 +15,26 @@ const saveItem = () => {
     label: newItem.value,
     highPriority: newItemPriority.value,
   });
-  newItem.value = "";
+  newItem.value = ""
+  newItemPriority.value = ""
+
 };
 const doEdit = (e) => {
   editing.value = e;
   newItem.value = "";
+  newItemPriority.value = ""
 };
+const togglePurchased = (item) => {
+  item.purchased = !item.purchased
+}
 </script>
 
 <template>
   <div class="header">
     <h1>{{ header }}</h1>
-    <button v-if="editing" class="btn" @click="doEdit(false)">Cancel</button>
+    <button v-if="editing" class="btn" @click="doEdit(false)">
+      Cancel
+    </button>
     <button v-else class="btn btn-primary" @click="doEdit(true)">
       Add Item
     </button>
@@ -34,7 +42,8 @@ const doEdit = (e) => {
   <form class="add-item" v-if="editing" @submit.prevent="saveItem">
     <input v-model.trim="newItem" type="text" placeholder="Add an item" />
     <label>
-      <input type="checkbox" v-model="newItemPriority" /> High Priority
+      <input type="checkbox" v-model="newItemPriority" /> 
+      High Priority
     </label>
     <button :disabled="newItem.length < 3" class="btn btn-primary">
       Save Item
@@ -42,12 +51,18 @@ const doEdit = (e) => {
   </form>
   <ul>
     <li
-      v-for="{ label, purchased, highpriority } in items"
+      v-for="(item, index) in items"
+      @click="togglePurchased(item)"
       class="static-class"
-      :class="{ strikeout: purchased, priority: highpriority }"
+      :class="{
+         strikeout: item.purchased,
+         priority: item.highpriority 
+      }"
     >
-      {{ label }}
+      {{ item.label }}
     </li>
   </ul>
-  <p v-if="!items.length">No Item has been added</p>
+  <p v-if="!items.length">
+  No Item has been added
+  </p>
 </template>
